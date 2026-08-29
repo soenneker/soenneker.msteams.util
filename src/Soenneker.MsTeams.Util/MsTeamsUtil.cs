1d240dee72a9610df7ac01ac3d58d47f7a6f0555
soenneker <jake@soenneker.com>
2026-08-29 14:50:56 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.MsTeams.Util/MsTeamsUtil.cs
+++ src/Soenneker.MsTeams.Util/MsTeamsUtil.cs
@@ -19,7 +19,7 @@ using Soenneker.Utils.Environment;
 
 namespace Soenneker.MsTeams.Util;
 
-///<inheritdoc cref="IMsTeamsUtil"/>
+/// <inheritdoc cref="IMsTeamsUtil"/>
 public sealed class MsTeamsUtil : IMsTeamsUtil
 {
     private readonly IConfiguration _config;
